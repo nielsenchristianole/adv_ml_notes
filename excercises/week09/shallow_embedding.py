cfb9ea5a6a944ed69ee0542886444015539c9872
Christian <nielsenchristianole@gmail.com>
2024-04-08 16:20:28 +0200

    random split

--- excercises/week09/shallow_embedding.py
+++ excercises/week09/shallow_embedding.py
@@ -75,7 +75,7 @@ def test(model: torch.nn.Module, idx_pairs: torch.Tensor, target: torch.Tensor) 
     return (pred == target).float().mean().item()
 
 
-def main(device: str, lr: float, max_step: int, n_splits: int, embedding_dim_space: list[int]):
+def main(device: str, lr: float, max_step: int, n_splits: int, embedding_dim_space: list[int], estimate_accuracy: bool = False):
     A = torch.load('data.pt', map_location=device)
     n_nodes = A.shape[0]
     idx_all_pairs = torch.triu_indices(n_nodes, n_nodes, 1, device=device)
@@ -83,26 +83,29 @@ def main(device: str, lr: float, max_step: int, n_splits: int, embedding_dim_spa
 
     cross_entropy = torch.nn.BCELoss().to(device)
 
-    acc = torch.empty((n_splits, len(embedding_dim_space)))
-    for split_idx, (train_idx, test_idx) in tqdm(enumerate(KFold(n_splits=n_splits).split(idx_all_pairs[0])), total=n_splits, desc='Splits'):
-        train_idx, test_idx = torch.tensor(train_idx), torch.tensor(test_idx)
+    if estimate_accuracy:
+        acc = torch.empty((n_splits, len(embedding_dim_space)))
+        for split_idx, (train_idx, test_idx) in tqdm(enumerate(KFold(n_splits=n_splits, shuffle=True).split(idx_all_pairs[0])), total=n_splits, desc='Splits'):
+            train_idx, test_idx = torch.tensor(train_idx), torch.tensor(test_idx)
 
-        train_idx_pairs = idx_all_pairs[:, train_idx]
-        test_idx_pairs = idx_all_pairs[:, test_idx]
+            train_idx_pairs = idx_all_pairs[:, train_idx]
+            test_idx_pairs = idx_all_pairs[:, test_idx]
 
-        train_target = target[train_idx]
-        test_target = target[test_idx]
+            train_target = target[train_idx]
+            test_target = target[test_idx]
 
-        for emb_idx, emb_dim in (p_bar := tqdm(enumerate(embedding_dim_space), total=len(embedding_dim_space), desc='Hyper params', leave=False)):
-            p_bar.set_description(f'emb_dim {emb_dim}')
+            for emb_idx, emb_dim in (p_bar := tqdm(enumerate(embedding_dim_space), total=len(embedding_dim_space), desc='Hyper params', leave=False)):
+                p_bar.set_description(f'emb_dim {emb_dim}')
 
-            model = Shallow(n_nodes, emb_dim).to(device)
-            optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+                model = Shallow(n_nodes, emb_dim).to(device)
+                optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-            train(model, optimizer, max_step, train_idx_pairs, train_target, cross_entropy)
-            accuracy = test(model, test_idx_pairs, test_target)
+                train(model, optimizer, max_step, train_idx_pairs, train_target, cross_entropy)
+                accuracy = test(model, test_idx_pairs, test_target)
 
-            acc[split_idx, emb_idx] = accuracy
+                acc[split_idx, emb_idx] = accuracy
+    else:
+        acc = torch.load('acc.pt', map_location=device)
 
 
     best_hyperparams_idx = acc.mean(0).argmax()
@@ -134,8 +137,13 @@ if __name__ == '__main__':
     parser.add_argument('--min_embedding_dim', default=3, type=int, help='minimum embedding dimension')
     parser.add_argument('--max_embedding_dim', default=7, type=int, help='maximum embedding dimension')
     parser.add_argument('--embedding_dim_space', nargs='+', type=int, help='list of embedding dimensions will override min_embedding_dim and max_embedding_dim')
-    parser.add_argument('--verbose', action='store_true', help='print verbose')
+    parser.add_argument('--estimate_accuracy', action='store_true', help='Estimate accuracy')
+
     args = parser.parse_args()
+
+    # print args
+    for arg in vars(args):
+        print(f'{arg}: {getattr(args, arg)}')
     
 
     device = args.device or 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -145,4 +153,4 @@ if __name__ == '__main__':
     embedding_dim_space = args.embedding_dim_space or list(range(args.min_embedding_dim, args.max_embedding_dim + 1))
 
 
-    main(device, lr, max_step, n_splits, embedding_dim_space)
+    main(device, lr, max_step, n_splits, embedding_dim_space, args.estimate_accuracy)
